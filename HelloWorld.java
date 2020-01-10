65d2dd846f7a6890d0987804510d32a3f2956c85
ChesterCoding <57189391+ChesterCoding@users.noreply.github.com>
2020-01-11 02:29:20 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,5 @@
 //************************测试小黑修改代码
+/*********************第二次测试********************************
 hello class HelloWorld {
 	public static void main(string[] args) {
 	Console.WriteLine("Hello, World!");
